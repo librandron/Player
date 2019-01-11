47d7129ac214cace1d33093ec284780b846654bb
Андрей М <librandron@gmail.com>
2019-01-11 20:32:09 +0300

    add shuffle and sort + extentions

---
+++ extentions/SongsExtentions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.extentions
+{
+    static class SongsExtentions
+    {
+        static public List<Song> Shuffle(this List<Song> songs)
+        {
+            Random rnd = new Random();
+            for (int i = songs.Count - 1; i <= 0; i--)
+            {
+
+                var song = songs[rnd.Next(songs.Count - 1)];
+                songs.Remove(song);
+                songs.Add(song);
+            }
+
+            return songs;
+        }
+
+    }
+}
